04c43eedc4c5e6c839dead6d235d4b769b51e493
Oleh Balabukh <o.balabuh.ob@hmail.com>
2023-11-23 20:40:16 +0200

    endaaa

---
+++ main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".Swiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+  });
